be3bb0df569236f2d60a9ff0709706bd4f391a23
Halle <francrv@hotmail.com>
2022-05-06 12:31:30 -0500

    chore: implement scroll inifinite to render data and use store

--- components/FilterTable.vue
+++ components/FilterTable.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <button @click="moreData"> mas </button>
     <Search :query="q" />
     <div v-if="isLoading">Loading data....</div>
     <div id="list">
@@ -11,10 +12,13 @@
 </template>
 
 <script>
+ import API from '../util/api'
+ let apiClass = new API();
 export default {
   name: 'FilterTable',
   data: () => ({
     users: [],
+    startFromPage: 0,
     isLoading: false,
   }),
   computed: {
@@ -29,11 +33,37 @@ export default {
   methods: {
     async fetchData() {
       this.isLoading = true;
-      const url = "/users.json";
-       const result  = await fetch(url).then((res => res.json()))
-       this.users = result.slice(0,600);
+       if (this.$store.state.users.listUsers.length == 0){
+          await apiClass.load();
+          this.users =  await apiClass.getUser(this.startFromPage);
+          this.$store.commit('users/append', this.users)
+       }
        this.isLoading = false;
 
+    },
+    async moreData(){
+       this.$store.commit('users/incrementStartFromPage', apiClass.getByPage()+1)
+       this.startFromPage = this.$store.state.users.startFromPage;
+       let newData = await apiClass.getUser(this.startFromPage);
+       this.$store.commit('users/append', newData);
+       this.users = this.$store.state.users.listUsers;
+    },
+    getNextUser() {
+      const listNode = document.getElementById("list")
+      listNode.onscroll = () => {
+        let bottomOfWindow = listNode.scrollTop + listNode.clientHeight >= listNode.scrollHeight ;
+        if (bottomOfWindow) {
+          this.moreData()
+        }
+      }
+    },
+  },
+  watch: {
+    users: {
+      handler(newValue, oldValue) {
+        this.users = newValue;
+      },
+      deep: true
     }
   },
   created() {
@@ -41,14 +71,19 @@ export default {
     this.$watch(
       () => this.$route.params,
       () => {
+        this.users = this.$store.state.users.listUsers;
+        this.startFromPage =  this.$store.state.users.startFromPage;
         this.fetchData()
       },
       // fetch the data when the view is created and the data is
       // already being observed
       { immediate: true }
     )
+  
   },
- 
+  mounted(){
+    this.getNextUser();
+  }
 }
 </script>
 
@@ -62,6 +97,7 @@ html, body {
   align-items: center;
   justify-items: center;
 }
+
 
 #app {
   font-family: 'Roboto', sans-serif;
